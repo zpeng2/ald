33ab18b0e97e730a12549657c98f08a75813defa
Zhiwei Peng <zhiweipeng1@gmail.com>
2020-12-03 18:11:18 -0800

    add example

--- examples/rtp_freespace_example.py
+++ examples/rtp_freespace_example.py
@@ -1,17 +1,23 @@
 import ald
 import numpy as np
+import h5py
 
-particle = ald.RTP(U0=1, tauR=1.0)
+U0 = 1.0
+tauR = 1.2
+particle = ald.RTP(U0=U0, tauR=tauR)
+
 flow = ald.ZeroVelocity()
 domain = ald.Box()
 
 
 ic = ald.InitialConfig(
-    x=ald.Point(0), y=ald.Uniform(-0.5, 0.5), theta=ald.Uniform(0, 2 * np.pi)
+    x=ald.Uniform(domain.left, domain.right),
+    y=ald.Uniform(domain.bottom, domain.top),
+    theta=ald.Uniform(0, 2 * np.pi),
 )
 
 
-cfg = ald.Config(particle, domain, N=204800, dt=1e-4, Nt=2000000)
+cfg = ald.Config(particle, domain, N=204800, dt=1e-4, Nt=4000000)
 
 kernel = ald.RTPFreespaceKernel()
 compiler = ald.RTPCompiler(kernel, cfg, flow, ic)
@@ -20,7 +26,10 @@ compiler.compile()
 
 simulator = ald.RTPSimulator(cfg, compiler)
 
-
+file = "U{:.3f}tauR{:.3f}.h5".format(U0, tauR)
+# create an empty file
+with h5py.File(file, "w") as f:
+    pass
 # range to compute stats on configuration and print time.
 runner = ald.RangedRunner(start=0, stop=cfg.Nt, freq=10000)
 # setup callbacks.
@@ -30,3 +39,9 @@ y = ald.DisplacementMeanVariance(runner, "y", unwrap=True)
 callbacks = [x, y, ald.ETA(runner)]
 
 simulator.run(cfg, callbacks=callbacks)
+
+# save particle, domain and simulation attributes.
+cfg.save2h5(file)
+# save mean variance of x
+x.save2h5(file, "x")
+y.save2h5(file, "y")
