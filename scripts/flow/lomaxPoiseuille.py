fix bug in wall fraction calculation

scripts/flow/lomaxPoiseuille.py
@@ -111,11 +111,12 @@ class WallFraction(ald.Callback):
 
     def compute_fraction(self, cfg):
         # get data.
-        y = getattr(cfg, "y")
+        y = getattr(cfg, "y").copy()
         # copy to cpu
         y = y.get()
-        Nw = np.sum([cfg.domain.bottom < val < cfg.domain.top for val in y])
-        fw = Nw / cfg.N
+        # number of particles inside the channel.
+        Nbulk = np.sum([cfg.domain.bottom < val < cfg.domain.top for val in y])
+        fw = 1 - Nbulk / cfg.N
         return fw
 
     def __call__(self, i, cfg):
@@ -136,7 +137,7 @@ class WallFraction(ald.Callback):
 
 if __name__ == "__main__":
     lH = [0.01, 0.1, 1]
-    #Peg = [0, 0.01, 0.1, 1, 10]
+    # Peg = [0, 0.01, 0.1, 1, 10]
     Peg = [1, 2, 10]
     for peg in Peg:
         simulate(lH=1.0, Peg=peg, alpha=1.2)